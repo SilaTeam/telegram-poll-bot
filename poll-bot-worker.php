8e2d0ce95dd8a9c74b4dbf599eb4507a4edff02f
Shahab <safaeishahab@yahoo.com>
2016-04-07 13:05:39 +0430

    Update poll-bot-worker.php

--- poll-bot-worker.php
+++ poll-bot-worker.php
@@ -4,7 +4,7 @@ set_time_limit(0);
 
 require_once 'PollBot.php';
 
-define('BOT_TOKEN', 'XXXXXXXXX:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX');
+define('BOT_TOKEN', '203026916:AAHFv8qCoeUIJpW6UbKbYKbXEDz2aUceIcM');
 
 $bot = new PollBot(BOT_TOKEN, 'PollBotChat');
 $bot->runLongpoll();
